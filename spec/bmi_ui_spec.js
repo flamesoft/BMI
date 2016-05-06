9bdc7ddd0836fc143483e2e12d0f01498142912e
Yan Zhang <emma2740@gmail.com>
2016-05-06 13:48:31 +0200

    Add test for Imperial method

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -3,16 +3,40 @@ describe('BMI_UI - index.html', function() {
     jasmine.getFixtures().fixturesPath = '.';
     loadFixtures('index.html');
     $.holdReady(false);
-    $('#weight').val('90');
-    $('#height').val('186');
-    $('#calculate').trigger('click');
   });
 
-  it("displays BMI value", function() {
-    expect($('#display_value').text()).toBe('Your BMI is 26.01');
+  describe('Metric method', function() {
+    beforeEach(function() {
+      $('#radio_metric').click();
+      $('#weight').val('90');
+      $('#height').val('186');
+      $('#calculate').trigger('click');
+    });
+
+    it("displays BMI value", function() {
+      expect($('#display_value').text()).toBe('Your BMI is 26.01');
+    });
+
+    it("displays BMI message", function() {
+      expect($('#display_message').text()).toBe('and you are Overweight');
+    });
+
   });
 
-  it("displays BMI message", function(){
-    expect($('#display_message').text()).toBe('and you are Overweight');
+  describe('Imperial method', function(){
+    beforeEach(function() {
+      $('#radio_imperial').click();
+      $('#weight').val('198');
+      $('#height').val('73');
+      $('#calculate').trigger('click');
+    });
+
+    it("displays BMI value", function() {
+      expect($('#display_value').text()).toBe('Your BMI is 26.12');
+    });
+
+    it("displays BMI message", function(){
+      expect($('#display_message').text()).toBe('and you are Overweight');
+    });
   });
 });
